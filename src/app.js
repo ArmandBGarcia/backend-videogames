dc09f2baf83a11592c387545ee48f45e0ec00c92
ArmandoBGarcia <xdarcx@hotmail.es>
2022-11-10 14:17:25 -0600

    configuring cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
-import cors from "cors";
+const cors = require("cors");
 
 require("./db.js");
 
